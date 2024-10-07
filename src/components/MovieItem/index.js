592a6fa1bda5a7a6ea3b09d576622d4b2f81d568
AZaid <zarreen2003@gmail.com>
2024-10-07 20:57:37 +0530

    Initial commit

---
+++ src/components/MovieItem/index.js
@@ -0,0 +1,37 @@
+import Popup from 'reactjs-popup';
+import ReactPlayer from 'react-player';
+import { IoClose } from 'react-icons/io5'; // Use IoClose instead of IoMdClose
+import 'reactjs-popup/dist/index.css';
+import './index.css';
+
+const MovieItem = ({ movieDetails }) => {
+  const { thumbnailUrl, videoUrl } = movieDetails;
+
+  return (
+    <div>
+      <Popup
+        modal
+        trigger={<img className="thumbnail" src={thumbnailUrl} alt="thumbnail" />}
+        className="popup-content"
+      >
+        {close => (
+          <div className="modal-container">
+            <button
+              className="close-button"
+              type="button"
+              data-testid="closeButton"
+              onClick={close}
+            >
+              <IoClose size={20} color="#231f20" /> {/* Updated icon */}
+            </button>
+            <div className="movie-player-container">
+              <ReactPlayer url={videoUrl} controls />
+            </div>
+          </div>
+        )}
+      </Popup>
+    </div>
+  );
+};
+
+export default MovieItem;
